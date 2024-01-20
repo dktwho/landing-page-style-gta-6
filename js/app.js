35ff8d73268a135e83bc123d16a9c7cd49d3c233
Alexander Korobov <dktwho@mail.ru>
2024-01-20 20:07:51 +0300

    feat: add settings basic for sliderMain

--- js/app.js
+++ js/app.js
@@ -1,3 +1,14 @@
 const sliderMain = new Swiper('.slider_main', {
-    freeMode: true
+    freeMode: true,
+    centeredSlides: true,
+    breakpoints: {
+        0: {
+            slidesPerView: 2.5,
+            spaceBetween: 20
+        },
+        680: {
+            slidesPerView: 3.5,
+            spaceBetween: 60
+        }
+    }
 })
